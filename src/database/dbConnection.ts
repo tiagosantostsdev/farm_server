60fbbe9b437762cba104ec113c3006a2769da83f
Tiago Santos <tiagossantostsdev@gmail.com>
2024-03-19 21:38:23 +0000

    added mongodb atlas config

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
+export const dbConnection = async () => {
   try {
     const mongodbURI:string = String(process.env.MONGODB_URI)
-    mongoose.connect(mongodbURI);
+    await mongoose.connect(mongodbURI);
     console.log({ message: "mongodb has been connected" });
   } catch (error) {
     if (error instanceof Error) {
