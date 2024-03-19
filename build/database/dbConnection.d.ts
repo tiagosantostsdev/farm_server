654175b22d65a5f8a68b6d1bfbe77ecdd4a1dd9f
Tiago Santos <tiagossantostsdev@gmail.com>
2024-03-19 21:40:00 +0000

    upadated build path

--- build/database/dbConnection.d.ts
+++ build/database/dbConnection.d.ts
@@ -1,1 +1,1 @@
-export declare const dbConnection: () => void;
+export declare const dbConnection: () => Promise<void>;
--- build/database/dbConnection.js
+++ build/database/dbConnection.js
@@ -1,14 +1,23 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnection = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const dbConnection = () => {
+const dbConnection = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const mongodbURI = String(process.env.MONGODB_URI);
-        mongoose_1.default.connect(mongodbURI);
+        yield mongoose_1.default.connect(mongodbURI);
         console.log({ message: "mongodb has been connected" });
     }
     catch (error) {
@@ -16,5 +25,5 @@ const dbConnection = () => {
             return console.error({ message: error.message });
         }
     }
-};
+});
 exports.dbConnection = dbConnection;
