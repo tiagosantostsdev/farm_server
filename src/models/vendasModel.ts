9e92ed835243ad6cbed66c81959c52821c48755d
Tiago Santos <tiagossantostsdev@gmail.com>
2024-03-20 01:41:27 +0000

    update v1

--- src/models/vendasModel.ts
+++ src/models/vendasModel.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const vendasSchema = new mongoose.Schema({
   nomeCliente: { type: String, required: true },
   produtos: { type: Array, required: true },
-  valor: { type: Number, default: 0 },
-  total: { type: Number, default: 0 },
+  valor: { type: Number },
+  total: { type: Number },
   troco: { type: Number, default: 0 },
   Funcionario: { type: mongoose.Schema.Types.ObjectId, ref: "funcionarios" },
   dataVenda: { type: String, required: true },
